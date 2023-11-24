9de6e421a23da6a419b586184652ea52591bd09a
liu_wanshun <liu_wanshun@qq.com>
2023-11-24 17:24:52 +0800

    tv denied rational style refer to AOSP PermissionController

--- permission-rationale/src/main/java/com/lws/permissionrationale/DefaultRationaleFactory.kt
+++ permission-rationale/src/main/java/com/lws/permissionrationale/DefaultRationaleFactory.kt
@@ -2,12 +2,19 @@ package com.lws.permissionrationale
 
 import android.app.Dialog
 import android.content.Context
+import android.content.res.Configuration
+import android.graphics.PixelFormat
+import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentDialog
+import androidx.core.content.ContextCompat
+import java.util.function.Consumer
 
 open class DefaultRationaleFactory : RationaleFactory {
     override fun createRationale(
@@ -42,7 +49,49 @@ open class DefaultRationaleFactory : RationaleFactory {
 
         return object : ComponentDialog(context, R.style.PermissionRationaleDialogTheme_Bottom) {
             init {
-                window?.setGravity(Gravity.BOTTOM)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && isTelevision(context)) {
+                    val window = window!!
+                    window.attributes = window.attributes.apply {
+                        flags = flags or WindowManager.LayoutParams.FLAG_BLUR_BEHIND
+                        blurBehindRadius = 20
+                        format = PixelFormat.TRANSPARENT
+                        verticalMargin = 0.03f
+                        gravity = Gravity.BOTTOM
+                    }
+
+                    val mBackgroundWithBlur = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.permissionrationale_background_with_blur
+                    )
+                    val mBackgroundNoBlur = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.permissionrationale_background
+                    )
+                    val mBackgroundBlurRadius = 80
+                    val blurEnabledListener =
+                        Consumer<Boolean> { enabled: Boolean ->
+                            window.setBackgroundDrawable(if (enabled) mBackgroundWithBlur else mBackgroundNoBlur)
+                            window.setBackgroundBlurRadius(if (enabled) mBackgroundBlurRadius else 0)
+                        }
+                    blurEnabledListener.accept(false)
+                    window.decorView.addOnAttachStateChangeListener(object :
+                        View.OnAttachStateChangeListener {
+
+
+                        override fun onViewAttachedToWindow(v: View) {
+                            window.windowManager.addCrossWindowBlurEnabledListener(
+                                blurEnabledListener
+                            )
+                        }
+
+                        override fun onViewDetachedFromWindow(v: View) {
+                            window.windowManager.removeCrossWindowBlurEnabledListener(
+                                blurEnabledListener
+                            )
+                        }
+                    })
+
+                }
             }
 
             override fun onCreate(savedInstanceState: Bundle?) {
@@ -84,5 +133,11 @@ open class DefaultRationaleFactory : RationaleFactory {
                 }
             }
         }
+    }
+
+
+    private fun isTelevision(context: Context): Boolean {
+        val uiMode = context.resources.configuration.uiMode
+        return uiMode and Configuration.UI_MODE_TYPE_MASK == Configuration.UI_MODE_TYPE_TELEVISION
     }
 }
